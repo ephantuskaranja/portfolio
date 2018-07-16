d9d67450eaf49e47850e188b4449210757c3416c
Ephantus karanja <ephantusslogan@gmail.com>
2018-07-16 07:35:57 +0300

    add home.html template

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ephantus/Desktop/Moringa_school_projects/Core-Projects/Django/Portfolio/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ virtual/lib/python3.6/heapq.py
@@ -0,0 +1,1 @@
+/home/ephantus/.pyenv/versions/3.6.5/lib/python3.6/heapq.py
